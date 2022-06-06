dc0fc340d5191853873acae0ad61f7114d5e0ab7
Tatiana Nikonova <taniushka_2007@mail.ru>
2022-06-06 09:50:25 +0300

    Initial

---
+++ src/components/Card.js
@@ -0,0 +1,34 @@
+function Card(props) {
+	if (!props) {
+		 return null;
+	}
+
+	const {img, title, content, ok, okTitle} = props;
+	const imgEl = img ? <img src={img} className="card-img-top" alt={img} /> : null;
+	let contentContainer = null;
+
+	if (title || content || ok) {
+		 const titleEl = title ? <h5 className="card-title">{title}</h5> : null;
+		 const contentEl = content ? <p className="card-text">{content}</p> : null;
+		 const okBtn = ok ? <a href={ok} className="btn btn-primary">{okTitle}</a> : null;
+
+		 contentContainer = <div className="card-body">
+			  {titleEl}
+			  {contentEl}
+			  {okBtn}
+		 </div>
+	}
+
+	return (
+		 <div className="card" style={{width: '18rem'}}>
+			  {imgEl}
+			  {contentContainer}
+		 </div>
+	)
+}
+
+Card.defaultProps = {
+	okTitle: 'Go somewhere',
+}
+
+export default Card;
